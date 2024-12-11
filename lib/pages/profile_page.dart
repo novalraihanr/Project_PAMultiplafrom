bc0800203ed338f3d111044ba18f3ae29b15196a
hanselabilo <hansel.abilo@gmail.com>
2024-12-11 10:27:35 +0700

    Update Navbar, Home, dan Profile

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -5,7 +5,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
@@ -27,11 +26,11 @@ class ProfilePage extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const Spacer(), 
+                  const Spacer(),
                   IconButton(
                     icon: SvgPicture.asset(
-                      'assets/icons/setting.svg',  
-                      width: 24.0,  
+                      'assets/icons/setting.svg',
+                      width: 24.0,
                       height: 24.0,
                     ),
                     onPressed: () {
@@ -41,26 +40,26 @@ class ProfilePage extends StatelessWidget {
                 ],
               ),
               const SizedBox(height: 30.0),
-
               Container(
-                padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+                padding: const EdgeInsets.symmetric(
+                    vertical: 15.0, horizontal: 20.0),
                 margin: const EdgeInsets.only(bottom: 20.0),
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  boxShadow: const [
+                  boxShadow: [
                     BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 5.0,
-                      offset: Offset(0, 4), 
+                      color: Colors.grey.withOpacity(0.5),
+                      blurRadius: 2.0,
+                      offset: Offset(0, 4),
                     ),
                     BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 5.0,
-                      offset: Offset(4, 0), 
+                      color: Colors.grey.withOpacity(0.5),
+                      blurRadius: 2.0,
+                      offset: Offset(4, 0),
                     ),
                     BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 5.0,
+                      color: Colors.grey.withOpacity(0.5),
+                      blurRadius: 2.0,
                       offset: Offset(-4, 0),
                     ),
                   ],
@@ -72,15 +71,14 @@ class ProfilePage extends StatelessWidget {
                     const CircleAvatar(
                       radius: 30.0,
                       backgroundImage: NetworkImage(
-                        'https://cdn-icons-png.flaticon.com/512/9385/9385289.png', 
+                        'https://cdn-icons-png.flaticon.com/512/9385/9385289.png',
                       ),
                     ),
-                  
                     const Expanded(
                       child: Padding(
-                        padding: EdgeInsets.only(left: 10.0), 
+                        padding: EdgeInsets.only(left: 10.0),
                         child: Text(
-                          'Nama', 
+                          'Nama',
                           style: TextStyle(
                             fontFamily: 'Poppins',
                             fontSize: 18.0,
@@ -89,13 +87,12 @@ class ProfilePage extends StatelessWidget {
                         ),
                       ),
                     ),
-
                     TextButton(
                       onPressed: () {
                         // INI LOGOUT
                       },
                       style: TextButton.styleFrom(
-                        backgroundColor: Colors.red, // Background merah
+                        backgroundColor: Colors.red,
                         padding: const EdgeInsets.symmetric(horizontal: 20.0),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10.0),
@@ -104,11 +101,10 @@ class ProfilePage extends StatelessWidget {
                       child: const Text(
                         'Logout',
                         style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Poppins',
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold
-                        ),
+                            color: Colors.white,
+                            fontFamily: 'Poppins',
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold),
                       ),
                     ),
                   ],
@@ -129,7 +125,6 @@ class ProfilePage extends StatelessWidget {
                 ],
               ),
               const SizedBox(height: 10.0),
-            
               GridView.builder(
                 padding: const EdgeInsets.symmetric(vertical: 10.0),
                 physics: const NeverScrollableScrollPhysics(),
